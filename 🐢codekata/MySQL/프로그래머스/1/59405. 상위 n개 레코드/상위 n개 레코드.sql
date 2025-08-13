825ecd2396c38afe1b6f897e2c83cd99f81127b2
Lim-git1 <67555302+Lim-git1@users.noreply.github.com>
2025-08-13 22:47:14 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 🐢codekata/MySQL/프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+SELECT name
+from animal_ins
+where datetime = (
+    select min(datetime)
+    from animal_ins
+);
+
+-- 코드를 입력하세요
+SELECT name
+from animal_ins
+order by datetime
+limit 1;
