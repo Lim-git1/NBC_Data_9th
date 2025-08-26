ba43333417154251a3e30d5334c2215a70be56a1
Lim-git1 <67555302+Lim-git1@users.noreply.github.com>
2025-08-26 11:41:01 +0900

    Update 즐겨찾기가 가장 많은 식당 정보 출력하기.sql

--- 🐢codekata/MySQL/프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 🐢codekata/MySQL/프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,4 +1,16 @@
 -- 코드를 입력하세요
+# 첫 제출안
+# XX <-- favorites만 비교하여서 최대 즐겨찾기 수와 즐겨찾기 수만 같다면, 다른 타입의 음식도 모두 출력됨
+select food_type, rest_id, rest_name, favorites
+from rest_info
+where favorites in (
+    select food_type, max(favorites)
+    from REST_INFO
+    group by FOOD_TYPE)
+GROUP BY REST_ID
+ORDER BY FOOD_TYPE desc
+
+# 수정안
 select food_type, rest_id, rest_name, favorites
 from rest_info
 where (food_type, favorites) in (
