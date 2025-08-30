67b051e669dab891e26f1c45ed73bf2326a9b5df
Lim-git1 <67555302+Lim-git1@users.noreply.github.com>
2025-08-30 12:51:25 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 🐢codekata/MySQL/프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT board_id, writer_id,title,price,
+CASE WHEN status = 'SALE' THEN '판매중'
+WHEN status = 'RESERVED' THEN '예약중'
+WHEN status = 'DONE' THEN '거래완료' END as STATUS
+FROM used_goods_board
+WHERE created_date like '%2022-10-05%'
+ORDER BY board_id desc
