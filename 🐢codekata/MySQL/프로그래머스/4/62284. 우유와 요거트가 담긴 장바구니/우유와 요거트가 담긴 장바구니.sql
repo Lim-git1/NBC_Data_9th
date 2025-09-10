e6231a7fe35dcc663fe2a089cf5bd21a653210f5
Lim-git1 <67555302+Lim-git1@users.noreply.github.com>
2025-09-10 09:34:38 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 🐢codekata/MySQL/프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 🐢codekata/MySQL/프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,5 +1,12 @@
--- 코드를 입력하세요
-SELECT CART_ID 
-FROM CART_PRODUCTS 
-GROUP BY CART_ID 
-HAVING MAX(NAME = 'Milk') AND MAX(NAME = 'Yogurt');
+
+-- 우유 혹은 요거트 구매한 장바구니 목록
+WITH L AS (
+    SELECT CART_ID, NAME
+    FROM CART_PRODUCTS
+    HAVING NAME IN ('Milk','Yogurt')
+)
+-- 둘다 구매한 장바구니
+SELECT CART_ID
+FROM L
+GROUP BY 1
+HAVING COUNT(DISTINCT NAME) >= 2 -- 둘다 구매
